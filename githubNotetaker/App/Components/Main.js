e5e76102a1ac00c4d140b8590b3e6be0704fe563
ijones16 <ijones16@github.com>
2015-04-18 13:04:19 -0700

    Example code for 2nd video

---
+++ githubNotetaker/App/Components/Main.js
@@ -0,0 +1,63 @@
+var React = require('react-native');
+
+var {
+  View,
+  Text,
+  StyleSheet
+} = React;
+
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#48BBEC'
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 25,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  searchInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+});
+
+class Main extends React.Component{
+  render() {
+    return(
+      <View style={styles.mainContainer}>
+      <Text> Testing the Router </Text>
+      </View>
+      )
+  }
+};
+
+module.exports = Main;
--- githubNotetaker/index.ios.js
+++ githubNotetaker/index.ios.js
@@ -1,53 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
+var Main = require('./App/Components/Main');
+
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View,
 } = React;
 
-var githubNotetaker = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Github Notetaker!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
-      </View>
-    );
-  }
+var styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#111111'
+  },
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class githubNotetaker extends React.Component{
+  render() {
+    return (
+      <NavigatorIOS
+      style={styles.container}
+        initialRoute={{
+          title: 'Github NoteTaker',
+          component: Main 
+        }} />
+    );
+  }
+};
+
 
 AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
